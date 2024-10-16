5005e6881d489c21097c565dd270229a8e9904a1
Pawar <amit.pawar@worldline.com>
2024-10-16 10:40:11 +0530

    app v2

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from GKE!');
+    res.send('Hello from GKE CICD! ');
 });
 
 app.listen(8080, () => {
